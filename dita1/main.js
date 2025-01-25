7b4eedbb3487b71f3d87ca2d893863494b6ce58b
Olti100 <oltiqerimi10@gmail.com>
2025-01-25 14:40:52 +0100

    Dita1

--- dita1/main.js
+++ dita1/main.js
@@ -1,1 +1,110 @@
-TextDecoderStream
+// var age = 20
+// var test= null;
+// var test2;
+// var X = "x"
+// var $njdnv;
+// var _test = 3
+
+var a = 50 + 50;
+
+console.log(a)
+console.log(50 + 50)
+
+var b = 20;
+var c = 30;
+var d = b + c;
+console.log(d)
+
+var e = 40;
+var f = 10;
+var g = e - f;
+console.log(g)
+
+var h = 100;
+var i = 5;
+var j = h / i;
+console.log(j)
+
+var k = 10;
+var l = 4;
+var m = k % l;
+console.log(m)
+
+var n = 99;
+console.log(n)
+
+n++;
+
+// n = n + 1;
+
+console.log(n)
+
+var o = 101;
+o--;
+console.log(o)
+
+var p = 20;
+var q = p ** 2;
+console.log(q)
+
+ var x = 15;
+ x+=5;
+ console.log(x)
+
+ var r = 35;
+ r-=5;
+ console.log(r)
+
+ var s = 10;
+ s*=7;
+ console.log(s)
+
+ var t = 200
+ t/=2
+ console.log(t)
+
+ var u = 10;
+ u%=5;
+ console.log(u)
+
+ var v = 5;
+ var w = 5;
+
+ console.log(v == w);
+
+ var x = 6;
+ var y = 7;
+
+ console.log(x != y)
+
+ var z = 100;
+ var num2 = 10;
+ console.log(z > 2)
+
+ var num3 = 1;
+ var num4 = 2;
+ console.log(num3 < 4)
+
+ var x = 123;
+ var y = "123";
+ console.log(x === y)
+
+ var firstName = "Digital";
+ var lastName = "School";
+
+ var fullName = firstName + " " +lastName;
+ console.log(fullName)
+
+ console.log("We are at" + fullName)
+ console.log(`Hello from ${fullName}`)
+
+ var age = 24;
+ console.log(age >= 18 && age <=65)
+
+ var var1 = 5;
+ var var2 = 6;
+ console.log(var1 == 5 || var2 ==5)
+
+ var var3 = 9;
+ var var4 = 10;
+ console.log(!(var3 == var4))
